97470c83ceab5591b278776224d83127492882fc
Narudom Techaval <narudom.techaval@thomsonreuters.com>
2015-02-22 21:48:45 +0700

    improve style

--- source/client/components/GameFinancePanel.jsx
+++ source/client/components/GameFinancePanel.jsx
@@ -3,10 +3,26 @@
 */
 'use strict';
 
-var React = require('react');
+var React = require('react/addons');
+var classSet = React.addons.classSet;
+
+var joinClasses = require('../../utilities/joinClasses');
 
 var GameFinancePanel = React.createClass({
   render: function () {
+    var weekSummary = -100;
+    var passiveIncome = 100;
+
+    var weekSummaryClasses = classSet({
+      'positive-block': weekSummary >= 0,
+      'negative-block': weekSummary < 0
+    });
+
+    var passiveIncomeClasses = classSet({
+      'positive-block': passiveIncome >= 0,
+      'negative-block': passiveIncome < 0
+    });
+
     return (
       <div>
         <div className="game-panel-header summary-header">
@@ -14,11 +30,21 @@ var GameFinancePanel = React.createClass({
         </div>
         <div className="game-panel-content summary-content">
           <ul className="list-group no-margin">
-            <li className="list-group-item">
-              Week Summary:
+            <li className="list-group-item inner-padding financial-block">
+              <span className="financial-title">
+                Week Summary:
+              </span>
+              <span className={ joinClasses(weekSummaryClasses, 'financial-figure pull-right') }>
+                { weekSummary }
+              </span>
             </li>
-            <li className="list-group-item">
-              Passive Income:
+            <li className="list-group-item inner-padding financial-block">
+              <span className="financial-title">
+                Passive Income:
+              </span>
+              <span className={ joinClasses(passiveIncomeClasses, 'financial-figure pull-right') }>
+                { passiveIncome }
+              </span>
             </li>
           </ul>
         </div>
