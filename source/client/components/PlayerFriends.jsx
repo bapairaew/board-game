fa70a33f095a72b7bf9e968e46a673e1af1ed737
Narudom Techaval <narudom.techaval@thomsonreuters.com>
2015-02-26 21:38:22 +0700

    player list

--- source/client/components/PlayerFriends.jsx
+++ source/client/components/PlayerFriends.jsx
@@ -3,14 +3,40 @@
 */
 'use strict';
 
-var React = require('react');
+var React = require('react/addons');
+var classSet = React.addons.classSet;
+
+var GameMixin = require('../mixins/GameMixin');
+
+var joinClasses = require('../../utilities/joinClasses');
 
 var PlayerFriends = React.createClass({
+  mixins: [GameMixin],
+
+  sortPlayers: function (a, b) {
+    return a.online ? -1 : 1;
+  },
+
+  getPlayerElements: function (players) {
+    return players.map(function (player) {
+      var playerClasses = classSet({
+        'online': player.online
+      });
+
+      return (
+        <li key={ player.id || 'undefined' } className={ joinClasses(playerClasses, 'player-list-item list-group-item inner-padding') }>
+          <span className="avatar"></span>
+          <span className="player-name">{ player.name }</span>
+        </li>
+      );
+    });
+  },
+
   render: function () {
     return (
-      <div>
-        Soon
-      </div>
+      <ul className="player-list list-group no-margin">
+        { this.getPlayerElements(this.state.environment.players.sort(this.sortPlayers)) }
+      </ul>
     );
   }
 });
