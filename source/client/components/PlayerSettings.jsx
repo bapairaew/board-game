5d53c6cd49a2827870b4450ab13252dd2812772f
Narudom Techaval <narudom.techaval@thomsonreuters.com>
2015-02-27 14:34:58 +0700

    mockup settings tab

--- source/client/components/PlayerSettings.jsx
+++ source/client/components/PlayerSettings.jsx
@@ -9,7 +9,33 @@ var PlayerSettings = React.createClass({
   render: function () {
     return (
       <div>
-        Soon
+        <div className="panel panel-default">
+          <div className="panel-heading">General</div>
+          <div className="panel-body">
+            <form>
+              <div className="form-group">
+                <label>Name</label>
+                <input type="email" className="form-control naked" />
+              </div>
+              <div className="form-group">
+                <label>Profile picture</label>
+                <br />
+                <a href>Change picture</a>
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <br />
+                <a href>Change Password</a>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="panel panel-default">
+          <div className="panel-heading">Actions</div>
+          <div className="panel-body">
+            <button className="btn btn-block btn-danger">Logout</button>
+          </div>
+        </div>
       </div>
     );
   }
